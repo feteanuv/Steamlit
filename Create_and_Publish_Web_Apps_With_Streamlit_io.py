02ffa7dc94047c7c50f6c73b39f47c8f9c8f0ca1
Vio <144432699+feteanuv@users.noreply.github.com>
2023-10-17 11:38:37 +0300

    Add files via upload

---
+++ Create_and_Publish_Web_Apps_With_Streamlit_io.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas
+
+
+data = {
+    'Series_1':[1, 2, 3, 4, 6],
+    'Series_2':[10, 20, 30, 50, 70]
+}
+
+df = pandas.DataFrame(data)
+
+st.title('My Fist Streamlit App')
+st.subheader('Introducing Streamlit in Automate Everything with Python')
+st.write('''This is our first Web App.
+
+Enjoy it!
+''')
+
+st.write(df)
+st.line_chart(df)
+
+#  to run app, we have to type in terminal 'streamlit run name.py'
